f580012c0679132828e245c8a9f64953827382c1
WHKL-9 <64627811+WHKL-9@users.noreply.github.com>
2021-08-25 14:35:02 +0200

    ex1 done

---
+++ m4d3/src/App.js
@@ -0,0 +1,13 @@
+import './App.css'
+import WarningSign from "./Components/WarningSign"
+
+
+function App() {
+  return (
+    <div>
+      <WarningSign name="Sara"/>
+    </div>
+  )
+}
+
+export default App;
