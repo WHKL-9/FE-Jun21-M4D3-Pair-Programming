85675545ea5ce00eab9277602925383f848bd94e
WHKL-9 <64627811+WHKL-9@users.noreply.github.com>
2021-08-25 15:46:31 +0200

    ex 3 completed

--- m4d3/src/Components/SingleBook.jsx
+++ m4d3/src/Components/SingleBook.jsx
@@ -2,24 +2,24 @@ import {Container, Card, Row, Col} from 'react-bootstrap'
 import fantasybooks from './Data/fantasy.json'
 import './SingleBook.css'
 
-const SingleBook =  () => (fantasybooks.map(book =>  {
-    <Container>
-            <Row className="justify-content-center">
-                <Col xs={12} sm={6} md={4} className="mb-2" key={book.asin}>
+const SingleBook =  () => {
+    return <Container>
+        <Row>
+            {
+                fantasybooks.map( book=> (
+                    <Col xs={12} sm={6} md={4} className="mb-2" key={book.asin}>
                     <Card id={book.asin} className="wholeCard">
                         <Card.Img variant="top" src={book.img} style={{height: "16rem" }} className="img-fluid" alt="bookimage"/>
                             <Card.Body >
                                 <Card.Title id="bookTitle">{book.title}</Card.Title>
                             </Card.Body>
                     </Card> 
-                </Col>
-            </Row>
+                </Col>  
+                ))
+            }
+        </Row>
     </Container>
 }
 
-)
-)
-
 export default SingleBook
-
 
