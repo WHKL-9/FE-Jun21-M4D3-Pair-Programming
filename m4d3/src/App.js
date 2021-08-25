8190347bf882722e9cdb5b91a7d9f2909cc23dd5
WHKL-9 <64627811+WHKL-9@users.noreply.github.com>
2021-08-25 15:32:12 +0200

    ex3 in progress

--- m4d3/src/App.js
+++ m4d3/src/App.js
@@ -1,6 +1,7 @@
 import './App.css'
 import MyBadge from './Components/MyBadge'
 import WarningSign from "./Components/WarningSign"
+import SingleBook from './Components/SingleBook'
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div>
       <WarningSign name="Sara"/>
       <MyBadge name="Weoy" color="primary"/>
+      <SingleBook/>
     </div>
   )
 }
